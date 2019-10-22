68fc301d3da8a5b265071cff635e4b2f6712fc00
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-22 06:47:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,16 @@
 def new_hash
   # return an empty hash
+  newHash = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  myHash = {"key", "value"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hashPioneer[:name] = "Grace Hopper"
 end
 
 def id_generator
